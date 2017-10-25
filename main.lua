08fbb4b2218b4373db079fea21503ba2dd7d1637
Robert Machmer <robert.machmer@gmail.com>
2017-10-25 19:07:37 +0200

    Fix splash issues

--- main.lua
+++ main.lua
@@ -1,25 +1,32 @@
 local o_ten_one = require "splash"
 
+local done
+
 local function mainGame()
 	love.graphics.setColor(0, 255, 0, 255)
 	love.graphics.print("Hello World!", 400, 300)
 	print("yoyoyo")
-end	
+end
 
 function love.load()
-  splash = o_ten_one({background=false})
-  splash.onDone = function() print("DONE") end
+	splash = o_ten_one({background=false})
+	splash.onDone = function() done = true end
 end
 
 function love.update(dt)
-  splash:update(dt)
+	if not done then
+		splash:update(dt)
+	end
 end
 
 function love.draw()
-  splash:draw()
-  mainGame()
+	if not done then
+		splash:draw()
+	else
+		mainGame()
+	end
 end
 
 function love.keypressed()
-  splash:skip()
+	splash:skip()
 end
